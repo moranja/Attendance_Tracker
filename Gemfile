be89705dc0d5cc32f4dc8c92004780f3c84f6a45
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-03-04 13:56:25 -0600

    adjusted /app/models/student.rb | modified Gemfile

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
+gem "date"
 gem "sinatra-activerecord"
 gem "sqlite3"
 gem "pry"
--- app/models/Student.rb
+++ app/models/Student.rb
@@ -3,6 +3,6 @@ class Student < ActiveRecord::Base
   has_many :school_days, through: :attendances
 
   def sign_in
+    Attendance.new(user_id: self, school_day_id: SchoolDay.last, arrival_time: DateTime.now, manually_changed: false)
   end
-
 end
