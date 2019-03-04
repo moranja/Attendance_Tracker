42688fc577ccf9e9a74a54ec895f7836aa6f4560
moranja <jeffadammoran@gmail.com>
2019-03-04 13:35:03 -0600

    added associations to Student, School_day

--- app/models/School_Day.rb
+++ app/models/School_Day.rb
@@ -1,3 +1,5 @@
 class SchoolDay < ActiveRecord::Base
-  
+  has_many :attendances
+  has_many :students, through: :attendances
+
 end
--- app/models/Student.rb
+++ app/models/Student.rb
@@ -1,3 +1,5 @@
 class Student < ActiveRecord::Base
-  
+  has_many :attendances
+  has_many :school_days, through: :attendances
+
 end
