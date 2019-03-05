9623bdc58ff5c69c5c99a124fa79001ff0fff7e7
moranja <jeffadammoran@gmail.com>
2019-03-05 10:25:18 -0600

    typos

--- app/Check_Attendance.rb
+++ app/Check_Attendance.rb
@@ -1,26 +1,27 @@
-require 'highline'
-require 'csv'
-require 'pry'
+# require 'highline'
+# require 'csv'
+# require 'pry'
 
 class Client < ActiveRecord::Base
 cli = HighLine.new
-input = cli.ask "Enter Your PIN number"
-result = Student.find_by(pin_number = input)
+#input = cli.ask "Enter Your PIN number"
+result = Student.find_by!(pin_number: (cli.ask "Enter Your PIN number"))
+binding.pry
 
-
-
+0
+end
 =begin
 x = []
 CSV.foreach("../csv/studentdb.csv") do |row|
   if row.include?(input)
-    x << row 
-  end 
+    x << row
+  end
 end
 binding.pry
 CSV.open("../csv/studentdb.csv") do |csv|
   csv.each do |row|
     if row.include?(input)
-      x << row 
+      x << row
     end
   end
 end
