5f070372f0db042c406733b94e9e4eb03e7bd0be
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-03-05 13:31:52 -0600

    modified cli

--- Check_Attendance.rb
+++ Check_Attendance.rb
@@ -3,20 +3,27 @@ require_relative 'config/environment.rb'
 cli = HighLine.new
 #input = cli.ask "Enter Your PIN number"
 
-$todays_date = DateTime.now.to_date.to_s
+#$todays_date = DateTime.now.to_date.to_s
 
-$today = School_Day.find_or_create_by(date: $todays_date)
+#$today = School_Day.find_or_create_by(date: $todays_date)
 
-$student = Student.find_by!(pin_number: (cli.ask "Enter Your PIN number"))
 
-#binding.pry
+$student = Student.find_by!(pin_number: (cli.ask "Welcome back to Flation! Please enter your PIN number: "))
+
+=begin
+if $student.is_admin == false
+puts "What would you like to do"
+1-Sign in
+$student.sign_in,
+2-check attendance record
+$student.check_attendance
+3-log out
+take you back to original menu
+=end
 
 $student.sign_in
 
-#Attendance.new(student: $student, school_day: School_Day.last, arrival_time: DateTime.now, manually_changed: false)
-
-
-
+puts "Hello #{$student.full_name} the time is: #{Attendance.last.arrival_time.to_time.strftime("%H:%M")}"
 
 
 Pry.start
