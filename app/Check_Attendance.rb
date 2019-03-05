80b17516d7aa695495ff85322c460b105d72d4fa
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-03-05 09:03:41 -0600

    Modified Gemfile | Check_Attendance & csv folder

--- app/Check_Attendance.rb
+++ app/Check_Attendance.rb
@@ -1,10 +1,21 @@
-require 'pry'
 require 'highline'
-
+require 'csv'
+require 'pry'
 cli = HighLine.new
-input = cli.ask "Enter your PIN number"
-if input == "1234"
-  puts "Swagger"
-else
-  puts "Not Swagger"
+input = cli.ask "Enter Your PIN number"
+x = []
+CSV.foreach("../csv/studentdb.csv") do |row|
+  if row.include?(input)
+    x << row 
+  end 
 end
+binding.pry
+=begin
+CSV.open("../csv/studentdb.csv") do |csv|
+  csv.each do |row|
+    if row.include?(input)
+      x << row 
+    end
+  end
+end
+=end
