eaaa59df6b785ca8c72bef035b09779a983b561b
moranja <jeffadammoran@gmail.com>
2019-03-07 13:14:12 -0600

    removed rspec

--- spec/student_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../app/models/Student.rb'
-
-describe Student do
-  let (:adam) {Student.create(full_name: 'Adam Moran', pin_number: '06211989'.to_i)}
-
-  it "Student should have a name" do
-    expect(adam.full_name).to(eq('Adam Moran'))
-  end
-end
